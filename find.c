f63ea9adc2073d27db06150d3c1a30c37c5c71a1
skarkal <karkalsaad@gmail.com>
2023-05-09 23:53:43 +0100

    Parsing..

---
+++ find.c
@@ -0,0 +1,30 @@
+#include "so_long.h"
+
+int __find(char	**map)
+{
+	int	i;
+	int	j;
+	t_var v;
+
+	i = -1;
+	v.c = 0;
+	v.e = 0;
+	v.p = 0;
+	while(map[++i])
+	{
+		j = 0;
+		while(map[i][j])
+		{
+			if (map[i][j] == 'C')
+				v.c++;
+			if (map[i][j] == 'P')
+				v.p++;
+			if (map[i][j] == 'E')
+				v.e++;
+			j++;
+		}
+	}
+	if(v.c == 0 || v.p != 1 || v.e != 1)
+		return (0);
+	return (1);
+}
