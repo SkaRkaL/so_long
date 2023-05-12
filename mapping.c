57f22386ee0aedbd87c7ee5962dbde9fed9e8626
skarkal <karkalsaad@gmail.com>
2023-05-12 23:56:42 +0100

    Parsing.

--- mapping.c
+++ mapping.c
@@ -1,6 +1,6 @@
 #include "so_long.h"
 
-static int	check_len(int *i, int len, char **map)
+static int check_len(int *i, int len, char **map)
 {
 	*i = 0;
 	while (map[*i])
@@ -12,9 +12,9 @@ static int	check_len(int *i, int len, char **map)
 	return (1);
 }
 
-static int	check_line(char **map, int i)
+static int check_line(char **map, int i)
 {
-	int	tmp_i;
+	int tmp_i;
 
 	tmp_i = 0;
 	while (map[0][tmp_i] == '1' && map[i - 1][tmp_i] == '1')
@@ -22,9 +22,9 @@ static int	check_line(char **map, int i)
 	return (tmp_i);
 }
 
-static int	check_space(char **map, int len)
+static int check_space(char **map, int len)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (map[i])
@@ -36,10 +36,10 @@ static int	check_space(char **map, int len)
 	return (1);
 }
 
-static int	check_p_c_e(char **map)
+static int check_p_c_e(char **map)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	i = 0;
 	while (map[i])
@@ -47,7 +47,7 @@ static int	check_p_c_e(char **map)
 		j = 0;
 		while (map[i][j])
 		{
-			if(map[i][j] != '0' && map[i][j] != '1' && map[i][j] != 'C' && map[i][j] != 'P' && map[i][j] != 'E')
+			if (map[i][j] != '0' && map[i][j] != '1' && map[i][j] != 'C' && map[i][j] != 'P' && map[i][j] != 'E')
 				return (0);
 			j++;
 		}
@@ -56,25 +56,25 @@ static int	check_p_c_e(char **map)
 	return (1);
 }
 
-int	map_check(char **map)
+int	map_check(t_data *map)
 {
 	int	i;
-	int	len;
-	int	count;
-	t_var v;
 
 	if (!map)
 		return (0);
-	len = ft_strlen(map[0]);
-	if (check_len(&i, len, map) == 0)
+	map->col = ft_strlen(map->s[0]);
+	if (check_len(&i, map->col, map->s) == 0)
 		return (0);
-	if (check_line(map, i) != len)
+	if (check_line(map->s, i) != map->col)
 		return (0);
-	if (check_space(map, len) == 0)
+	if (check_space(map->s, map->col) == 0)
 		return (0);
-	if (check_p_c_e(map) == 0)
+	if (check_p_c_e(map->s) == 0)
 		return (0);
-	if (__find(map) == 0)
+	if (__find(map->s, map) == 0)
 		return (0);
-	return 1;
+	get_p_pos(map->s, map);
+	// if (valid_path(map, map) == 0)
+	// 	return (0);
+	return (1);
 }
