f78dace785f5e17882f2454bba2c2313dabd043e
skarkal <karkalsaad@gmail.com>
2023-05-22 02:10:46 +0100

    Done

--- window.c
+++ window.c
@@ -8,8 +8,6 @@ void	*mlx_window(t_data *map)
 
 	w = map->width;
 	h = map->height;
-	if (w > 2624 || h > 1408)
-		exit(write(1, "Error\n", 6));
 	ptr = mlx_new_window(map->mlx_p, w, h, T);
 	if (!ptr)
 		exit(write(2, "Error Window\n", 13));
